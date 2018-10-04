e45efd6e7be5274980ed533cf131c7603baed0e7
Lis212 <b.l.samoylov@gmail.com>
2018-10-05 00:32:06 +0300

    Add Hibernate in project

---
+++ src/main/java/io/khasang/ba/entity/User.java
@@ -0,0 +1,40 @@
+package io.khasang.ba.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private long id;
+
+    private String name;
+    private String speciality;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+}
